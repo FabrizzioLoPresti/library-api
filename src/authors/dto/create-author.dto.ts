2a8630031bb7188706c93fd7be09c29f5da9fb07
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2023-11-03 08:53:49 -0300

    Tags and DTOs Documentation in Swagger

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateAuthorDto {
+  @ApiProperty({ description: 'Name of the author' })
   name: string;
 }
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,8 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AuthorDto {
+  @ApiProperty({ description: 'Author ID' })
+  authorId: number;
+}
+
 export class CreateBookDto {
+  @ApiProperty({ description: 'Title of the book' })
   title: string;
+
+  @ApiProperty({ description: 'Number of chapters in the book' })
   chapters: number;
+
+  @ApiProperty({ description: 'Total number of pages in the book' })
   pages: number;
-  authors: {
-    authorId: number;
-  }[];
+
+  @ApiProperty({
+    description: 'Array of authors for the book',
+    type: [AuthorDto],
+  })
+  authors: AuthorDto[];
 }
